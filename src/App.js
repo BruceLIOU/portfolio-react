b240474573e1b2201b40db0c3db4f8d7d2bb4623
Bruce LIOU <bruceliou@free.fr>
2021-08-30 17:23:48 +0200

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./assets/css/App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/containers/README.md
@@ -0,0 +1,3 @@
+# Only for add this folder into repository
+
+After created your app React, you can delete this README.md
