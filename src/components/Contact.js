9aeb59fb931e3aaf8133354370f7c022a3703385
Bruce LIOU <bruceliou@free.fr>
2022-08-10 10:41:04 +0200

    change google mapas embed & hide address

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -122,7 +122,7 @@ const Contact = () => {
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
         <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11508.16461873702!2d4.160053279458995!3d43.85497566051495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b424a4c75a5ccd%3A0x7d54ec6e63281620!2s24%20Rue%20de%20l&#39;%20Ancienne%20Cave%2C%2030730%20Montpezat!5e0!3m2!1sfr!2sfr!4v1630407794990!5m2!1sfr!2sfr"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22988.432496206628!2d4.247353768369119!3d43.92720576321408!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b430db4775c8c7%3A0x4078821166b44d0!2s30190%20La%20Calmette!5e0!3m2!1sfr!2sfr!4v1660119579343!5m2!1sfr!2sfr"
             width="100%"
             height="100%"
             title="map"
@@ -137,10 +137,7 @@ const Contact = () => {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 Adresse
               </h2>
-              <p className="mt-1">
-                24 rue de l'ancienne cave <br />
-                30 730 Montpezat
-              </p>
+              <p className="mt-1">30190 La Calmette</p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
@@ -148,8 +145,7 @@ const Contact = () => {
               </h2>
               <a
                 href="mailto:contact@bruceliou.fr"
-                className="text-indigo-400 leading-relaxed"
-              >
+                className="text-indigo-400 leading-relaxed">
                 contact@bruceliou.fr
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
@@ -163,8 +159,7 @@ const Contact = () => {
           method="POST"
           netlify="true"
           name="contact"
-          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
-        >
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Contactez-moi
           </h2>
@@ -204,8 +199,7 @@ const Contact = () => {
           <div className="relative mb-4">
             <label
               htmlFor="message"
-              className="leading-7 text-sm text-gray-400"
-            >
+              className="leading-7 text-sm text-gray-400">
               Message *
             </label>
             <textarea
